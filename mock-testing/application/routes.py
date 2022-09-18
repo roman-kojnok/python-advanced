6c540561764aae5b6f1238ee63f6bd5c5021ef7c
roman-kojnok <roman.kojnok@protonmail.com>
2022-09-18 16:12:25 +0100

    Modified code

--- mock-testing/application/routes.py
+++ mock-testing/application/routes.py
@@ -5,6 +5,20 @@ import requests
 
 @app.route('/get/sport', methods=['GET'])
 def sport():
+    number = requests.get('http://api:5000/get/number').text
+    letter = requests.get('http://api:5000/get/letter').text
+    if number == "1" and letter == "a":
+        return "Football"
+    elif number == "1" and letter == "b":
+        return "Badminton"
+    elif number == "1" and letter == "c":
+        return "Hockey"
+    else:
+        return "Boxing"
+
+
+"""@app.route('/get/sport', methods=['GET'])
+def sport():
     response = requests.get('http://api:5000/get/number')
     if response.text == "1":
         return "Football"
@@ -13,4 +27,6 @@ def sport():
     elif response.text == "3":
         return "Hockey"
     else:
-        return "Boxing"
+        return "Boxing"""
+
+
