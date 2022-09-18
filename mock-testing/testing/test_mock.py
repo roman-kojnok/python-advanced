3f184682458b6093d4639ce41dcb0e97ca4e9c98
roman-kojnok <roman.kojnok@protonmail.com>
2022-09-18 16:44:10 +0100

    100% test

--- mock-testing/testing/test_mock.py
+++ mock-testing/testing/test_mock.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
-from unittest.mock import patch
+# from unittest.mock import patch
 from flask import url_for
 from flask_testing import TestCase
-
+import requests_mock
 from application import app
 
 
@@ -12,11 +12,50 @@ class TestBase(TestCase):
 
 
 class TestResponse(TestBase):
+       def test_football(self):
+        with requests_mock.Mocker() as g:
+            g.get('http://api:5000/get/number', text='1')
+            g.get('http://api:5000/get/letter', text='a')
 
-    def test_football(self):
+            response = self.client.get(url_for('sport'))
+            self.assertIn(b'Football', response.data)
+
+
+class TestResponse2(TestBase):
+    def test_Badminton(self):
+        with requests_mock.Mocker() as g:
+            g.get('http://api:5000/get/number', text='1')
+            g.get('http://api:5000/get/letter', text='b')
+
+            response = self.client.get(url_for('sport'))
+            self.assertIn(b'Badminton', response.data)
+
+
+class TestResponse3(TestBase):
+    def test_Hockey(self):
+        with requests_mock.Mocker() as g:
+            g.get('http://api:5000/get/number', text='1')
+            g.get('http://api:5000/get/letter', text='c')
+
+            response = self.client.get(url_for('sport'))
+            self.assertIn(b'Hockey', response.data)
+
+
+class TestResponse4(TestBase):
+    def test_Boxing(self):
+        with requests_mock.Mocker() as g:
+            g.get('http://api:5000/get/number', text='1')
+            g.get('http://api:5000/get/letter', text='d')
+
+            response = self.client.get(url_for('sport'))
+            self.assertIn(b'Boxing', response.data)
+
+
+"""    def test_football(self):
         # We will mock a response of 1 and test that we get football returned.
         with patch('requests.get') as g:
             g.return_value.text = "1"
 
             response = self.client.get(url_for('sport'))
-            self.assertIn(b'Football', response.data)
+            self.assertIn(b'Football', response.data)"""
+
